0d76611674aef5b0f0a133ced33945f8adb4984c
Krishna <softeng.krishna@gmail.com>
2016-02-20 12:54:03 +0530

    Initial commit

---
+++ app/src/main/java/visionpinnacle/edubuzz/com/circlemenu/MainActivity.java
@@ -0,0 +1,22 @@
+package visionpinnacle.edubuzz.com.circlemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] mItemTexts = new String[] { "Blood Bank", "Result", "Settings",
+            "Clubs", "Share App", "About Us" };
+    private int[] mItemImgs = new int[] { R.drawable.ic_blood,
+            R.drawable.icon_result, R.drawable.icon_setting,
+            R.drawable.icon_reports, R.drawable.icon_share,
+            R.drawable.icon_aboutus };
+    CircleMenuLayout mCircleMenuLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
+    }
+}
